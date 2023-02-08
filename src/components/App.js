578dd3355145ed0cca2cba9e02fc537232e8052e
Diana Lin <linjdiana@gmail.com>
2023-02-08 13:44:57 -0800

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
-function App() {
+function App({ updateSearch }) {
+
+  const [listings, setListings] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((resp) => resp.json())
+      .then((listings) => setListings(listings));
+  }, []);
+
+  function removeListing(id) {
+    const newListingsArray = listings.filter((listing) => listing.id!== id);
+    setListings(newListingsArray);
+  }
+
+  function updateSearch(searchValue) {
+    setSearchTerm(searchValue);
+  }
+
+  const filteredListings = listings.filter((listing) => {
+    const lowerCaseDescription = listing.description.toLowerCase();
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    return lowerCaseDescription.includes(lowerCaseSearchTerm);
+  });
+
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header searchTerm={searchTerm} updateSearch={updateSearch} />
+      <ListingsContainer
+        listings={filteredListings}
+        removeListing={removeListing}
+      />
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,19 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ listings, removeListing, search }) {
+
+  const listingCards = listings.map((listing) => {
+    return <ListingCard
+    key={listing.id}
+    listing={listing}
+    removeListing={removeListing} />;
+  });
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
